f1ee1eadd947ef19fab705348adaf2eb13dfdef4
Sebastian Jambor <seb.jambor@gmail.com>
2023-01-04 19:35:24 +0100

    extending functionality of audience helper

--- app/lib/activity_log_audience_helper.rb
+++ app/lib/activity_log_audience_helper.rb
@@ -3,10 +3,12 @@
 class ActivityLogAudienceHelper
 
   def self.audience(activity_log_event)
+    domain = Rails.configuration.x.local_domain
+
     if activity_log_event.type == 'outbound'
       actor = activity_log_event.data['actor']
 
-      if actor and match = actor.match(Regexp.new("https://#{Rails.configuration.x.local_domain}/users/(.*)"))
+      if actor and match = actor.match(Regexp.new("https://#{domain}/users/(.*)"))
         return [match.captures[0]]
       else
         return []
@@ -14,13 +16,15 @@ class ActivityLogAudienceHelper
     end
 
     if activity_log_event.type == 'inbound'
-      if match = activity_log_event.path.match(Regexp.new("https://#{Rails.configuration.x.local_domain}/users/(.*)/inbox"))
+      if match = activity_log_event.path.match(Regexp.new("https://#{domain}/users/(.*)/inbox"))
         return [match.captures[0]]
+      elsif activity_log_event.path == "https://#{domain}/inbox"
+
       end
 
       return []
     end
 
-    return ['admin']
+    return []
   end
 end
