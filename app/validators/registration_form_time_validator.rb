9ae47dbc1874f1edf884db7689e4f53a19e22029
Sebastian Jambor <seb.jambor@gmail.com>
2023-01-10 18:49:59 +0100

    allow shorter time for sign up

--- app/validators/registration_form_time_validator.rb
+++ app/validators/registration_form_time_validator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class RegistrationFormTimeValidator < ActiveModel::Validator
-  REGISTRATION_FORM_MIN_TIME = 3.seconds.freeze
+  REGISTRATION_FORM_MIN_TIME = 0.5.seconds.freeze
 
   def validate(user)
     user.errors.add(:base, I18n.t('auth.too_fast')) if user.registration_form_time.present? && user.registration_form_time > REGISTRATION_FORM_MIN_TIME.ago
