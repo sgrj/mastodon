a3b8fa244a33f8b8645e980e7b7ee139fc0f938e
Sebastian Jambor <seb.jambor@gmail.com>
2023-01-12 19:48:04 +0100

    allow multiple clients for the same id

---
+++ spec/lib/activity_logger_spec.rb
@@ -0,0 +1,58 @@
+require 'json'
+require 'rails_helper'
+
+def activity_log_event_fixture(name)
+  json_string = File.read(Rails.root.join('spec', 'fixtures', 'activity_log_events', name))
+
+  ActivityLogEvent.from_json_string(json_string)
+end
+
+RSpec.describe ActivityLogger do
+
+  after(:each) do
+    ActivityLogger.reset
+  end
+
+  describe 'log' do
+
+    it 'sends events to all listeners of the same id' do
+      sse1 = spy('sse1')
+      sse2 = spy('sse2')
+      event = double('event')
+
+      ActivityLogger.register('test_id', sse1)
+      ActivityLogger.register('test_id', sse2)
+
+      ActivityLogger.log('test_id', event)
+
+      expect(sse1).to have_received(:write).with(event)
+      expect(sse2).to have_received(:write).with(event)
+    end
+
+    it 'does not send events to listeners of a different id' do
+      sse = spy('sse')
+      event = double('event')
+
+      ActivityLogger.register('test_id', sse)
+
+      ActivityLogger.log('other_id', event)
+
+      expect(sse).to_not have_received(:write).with(event)
+    end
+
+    it 'does not send events to listeners after unregistering' do
+      sse1 = spy('sse1')
+      sse2 = spy('sse2')
+      event = double('event')
+
+      ActivityLogger.register('test_id', sse1)
+      ActivityLogger.register('test_id', sse2)
+      ActivityLogger.unregister('test_id', sse1)
+
+      ActivityLogger.log('test_id', event)
+
+      expect(sse1).to_not have_received(:write).with(event)
+      expect(sse2).to have_received(:write).with(event)
+    end
+  end
+end
