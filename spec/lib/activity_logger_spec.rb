240160f877beaff12f1a6f5a56ea1c5e64c55917
Sebastian Jambor <seb.jambor@gmail.com>
2023-01-14 17:12:20 +0100

    fix issue in activity logger

--- spec/lib/activity_logger_spec.rb
+++ spec/lib/activity_logger_spec.rb
@@ -29,6 +29,21 @@ RSpec.describe ActivityLogger do
       expect(sse2).to have_received(:write).with(event)
     end
 
+    it 'sends events to all listeners even if some fail' do
+      sse1 = spy('sse1')
+      sse2 = spy('sse2')
+      event = double('event')
+
+      allow(sse1).to receive(:write).and_throw(:error)
+
+      ActivityLogger.register('test_id', sse1)
+      ActivityLogger.register('test_id', sse2)
+
+      ActivityLogger.log('test_id', event)
+
+      expect(sse2).to have_received(:write).with(event)
+    end
+
     it 'does not send events to listeners of a different id' do
       sse = spy('sse')
       event = double('event')
