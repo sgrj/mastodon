d484826923b32060398414fb98b9312cdd9c804f
Sebastian Jambor <seb.jambor@gmail.com>
2023-01-27 17:47:50 +0100

    links in activity log open explorer

--- app/javascript/mastodon/actions/activitypub_explorer.js
+++ app/javascript/mastodon/actions/activitypub_explorer.js
@@ -1,6 +1,12 @@
 export const ACTIVITYPUB_EXPLORER_DATA ='ACTIVITYPUB_EXPLORER_DATA';
+export const ACTIVITYPUB_EXPLORER_URL ='ACTIVITYPUB_EXPLORER_URL';
 
 export const setExplorerData = (value) => ({
   type: ACTIVITYPUB_EXPLORER_DATA,
+  value,
+});
+
+export const setExplorerUrl = (value) => ({
+  type: ACTIVITYPUB_EXPLORER_URL,
   value,
 });
--- app/javascript/mastodon/reducers/activitypub_explorer.js
+++ app/javascript/mastodon/reducers/activitypub_explorer.js
@@ -1,14 +1,17 @@
 import { Map as ImmutableMap } from 'immutable';
-import { ACTIVITYPUB_EXPLORER_DATA } from '../actions/activitypub_explorer';
+import { ACTIVITYPUB_EXPLORER_DATA, ACTIVITYPUB_EXPLORER_URL } from '../actions/activitypub_explorer';
 
 const initialState = ImmutableMap({
   data: null,
+  url: '',
 });
 
 export default function activitypub_explorer(state = initialState, action) {
   switch (action.type) {
   case ACTIVITYPUB_EXPLORER_DATA:
     return state.set('data', action.value);
+  case ACTIVITYPUB_EXPLORER_URL:
+    return state.set('url', action.value);
   default:
     return state;
   }
