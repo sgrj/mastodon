7ee124508e22383dacd90999a94149a5ea009436
Sebastian Jambor <seb.jambor@gmail.com>
2023-07-03 19:56:10 +0200

    catch exceptions in activity subscriber

--- lib/activity_log_subscriber.rb
+++ lib/activity_log_subscriber.rb
@@ -7,10 +7,14 @@ class ActivityLogSubscriber
 
     redis.subscribe('activity_log') do |on|
       on.message do |channel, message|
-        event = ActivityLogEvent.from_json_string(message)
+        begin
+          event = ActivityLogEvent.from_json_string(message)
 
-        ActivityLogAudienceHelper.audience(event)
-          .each { |username| ActivityLogger.log(username, event) }
+          ActivityLogAudienceHelper.audience(event)
+            .each { |username| ActivityLogger.log(username, event) }
+        rescue => e
+          Rails.logger.error (["Error parsing #{message}. #{e.class}: #{e.message}"]+e.backtrace).join("\n")
+        end
       end
     end
   end
