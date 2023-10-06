fb727ee28c9e31dc1512afe9b0c546401f173343
Sebastian Jambor <seb.jambor@gmail.com>
2023-10-06 21:42:48 +0200

    allow sending of arbitrary activities

--- app/controllers/api/v1/activity_controller.rb
+++ app/controllers/api/v1/activity_controller.rb
@@ -4,6 +4,18 @@ class Api::V1::ActivityController < Api::BaseController
   before_action :authenticate_user!
 
   def create
-    render json: current_account
+    ActivityPub::DeliveryWorker.perform_async(Oj.dump(activity_params[:activity]), current_account.id, activity_params[:inbox_url])
+
+    # to allow sending of activities without a user (to develop the frontend),
+    # uncomment the following line, and comment the authenticate_user before_action
+    # ActivityPub::DeliveryWorker.perform_async(Oj.dump(activity_params[:activity]), 110819192012359728, activity_params[:inbox_url])
+    render json: true
+  end
+
+
+  # TODO: require inbox_url and activity
+  def activity_params
+    # params.permit(:inbox_url, :activity)
+    params
   end
 end
